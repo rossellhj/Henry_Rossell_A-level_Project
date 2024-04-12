28d36b338f4327369723dca93f80fda9135fc2bd
neptunestar999 <60449109+neptunestar999@users.noreply.github.com>
2024-04-12 13:14:45 +0100

    player.png + more menus

--- project.py
+++ project.py
@@ -19,6 +19,12 @@ pygame.display.set_caption("")
 
 done = False
 state="Main Menu"
+health=0
+
+def text(size, text, posx, posy):
+    font = pygame.font.SysFont('courier', size, True, False)
+    text = font.render((text),True,BLACK)
+    screen.blit(text, [posx, posy])
 
 clock = pygame.time.Clock()
 
@@ -27,21 +33,34 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if state == "Main Menu":
+                    state = "Select team"
+                elif state == "Select team":
+                    state = "Select level"
+                elif state == "Select level":
+                    state = "Level 1"
             
     screen.fill(BLUE)
 
     if state == "Main Menu":
-        font = pygame.font.SysFont('courier', 40, True, False)
-        text = font.render(("Main Menu"),True,BLACK)
-        screen.blit(text, [240, 50])
+        text(40, "Main Menu", 240, 50)
+        text(25, "Select Team", 275, 200)
+        text(25, "Highscores", 280, 250)
+        
+    if state == "Select team":
+        text(40, "Select Team", 240, 50)
+        text(20, "Drake", 290, 200)
+        text(20, "Grenville", 260, 240)
 
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    state = "Select team"
+    if state == "Select level":
+        text(40, "Select Level", 240, 50)
 
-
-    
+    if state == "Level 1":
+        text(15, "Health:", 20, 10)
+        text(15, str(health), 100,10)
+        text(15, "Timer:", 600, 10)
         
  
     pygame.display.flip()
